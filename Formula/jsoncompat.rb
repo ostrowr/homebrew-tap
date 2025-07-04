f5de2fc810ac16b2aa8eebcbc89304b1e205259f
Robbie Ostrow <ostrowr@users.noreply.github.com>
2025-07-03 22:21:04 -0700

    jsoncompat 0.2.0

--- Formula/jsoncompat.rb
+++ Formula/jsoncompat.rb
@@ -1,17 +1,17 @@
 class Jsoncompat < Formula
   desc "Escape version skew"
   homepage "https://github.com/ostrowr/jsoncompat"
-  version "0.1.5"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ostrowr/jsoncompat/releases/download/v#{version}/jsoncompat-macos-aarch64"
-      sha256 "ef6e5628945b2680c0d6389d80cb2a7bb6621302815aea18a989f684a3a922b4"
+      sha256 "f884173c03925204a642fe038b98ee91b2e1ac2b6a988a6f91b6485b82821ada"
       @binary_name = "jsoncompat-macos-aarch64"
     elsif Hardware::CPU.intel?
       url "https://github.com/ostrowr/jsoncompat/releases/download/v#{version}/jsoncompat-macos-x86_64"
-      sha256 "7ff7cd2485244a14faccf11e2b3d949ace36f5e2f655b55b70a31632d5d32964"
+      sha256 "0ce61f418ebc741637ce55bcca22cae4a1baea75e162c77c32c9345549e44020"
       @binary_name = "jsoncompat-macos-x86_64"
     end
   end
@@ -19,7 +19,7 @@ class Jsoncompat < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/ostrowr/jsoncompat/releases/download/v#{version}/jsoncompat-linux-x86_64"
-      sha256 "c24e6adae82fa4f7ec5d834f4bcc50e85cbe83df861b62439fb9b74a9f2c38ba"
+      sha256 "d246e6f6ed1f494cb37b233fd2237fce6cfb81846c505429fdd85f56e798dad3"
       @binary_name = "jsoncompat-linux-x86_64"
     end
   end
